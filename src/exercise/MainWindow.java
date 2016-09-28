e21306d24d94a92653c9ac0f91589d31c16c3555
oop <oop@pc01>
2016-09-28 10:09:03 -0500

    widgets

--- src/exercise/MainWindow.java
+++ src/exercise/MainWindow.java
@@ -5,8 +5,11 @@
  */
 package exercise;
 
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 import javax.swing.JDesktopPane;
 import javax.swing.JFrame;
+import javax.swing.JInternalFrame;
 import javax.swing.JMenu;
 import javax.swing.JMenuBar;
 import javax.swing.JMenuItem;
@@ -18,28 +21,63 @@ import javax.swing.JMenuItem;
 public class MainWindow extends JFrame
 {
     private JMenuBar menu;
-    private JMenu fiel, edit, exercises,help;
+    private JMenu file, edit, exercises,help;
     private JMenuItem open, quit;
     private JDesktopPane mdi;
     //--------------------------------------------------------------------------
         
     public MainWindow()
     {
+        super("Application MDI");
+        setSize(600, 400);
+        widgets();
+        events();
+        setVisible(true);       
         
     }
     //--------------------------------------------------------------------------
     private void widgets()
     {
+        menu = new JMenuBar();
+        file = new JMenu("FIle");
+        edit = new JMenu("Edit");
+        help = new JMenu("Help");
+        open = new JMenuItem("Open");
+        quit = new JMenuItem("Exit");
+        mdi = new JDesktopPane();
+        
+        setJMenuBar(menu);
+        menu.add(file);
+        menu.add(edit);
+        menu.add(help);        
+        file.add(open);
+        file.addSeparator();
+        file.add(quit);
+        
+        add(mdi);       
+        
+        
         
     }
     //--------------------------------------------------------------------------
     private void events()
     {
+        setDefaultCloseOperation(EXIT_ON_CLOSE);
+        //----------------------------------------------------------------------
+        open.addActionListener(new ActionListener(){
+            public void  actionPerformed(ActionEvent e){
+                JInternalFrame child = new JInternalFrame("Untitle 1");
+                child.setSize(400, 300);
+                child.setVisible(true);
+                mdi.add(child);
+            }            
+        });
         
     }
     //--------------------------------------------------------------------------
     public static void main(String[] args) 
     {
+        new MainWindow();
         
     }
     
