8e61f34983ec92ff4a3d54fe737e0ff2831db284
oop <oop@pc01>
2016-09-28 09:37:05 -0500

    Primera modificacion

---
+++ src/exercise/MainWindow.java
@@ -0,0 +1,46 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package exercise;
+
+import javax.swing.JDesktopPane;
+import javax.swing.JFrame;
+import javax.swing.JMenu;
+import javax.swing.JMenuBar;
+import javax.swing.JMenuItem;
+
+/**
+ *
+ * @author oop
+ */
+public class MainWindow extends JFrame
+{
+    private JMenuBar menu;
+    private JMenu fiel, edit, exercises,help;
+    private JMenuItem open, quit;
+    private JDesktopPane mdi;
+    //--------------------------------------------------------------------------
+        
+    public MainWindow()
+    {
+        
+    }
+    //--------------------------------------------------------------------------
+    private void widgets()
+    {
+        
+    }
+    //--------------------------------------------------------------------------
+    private void events()
+    {
+        
+    }
+    //--------------------------------------------------------------------------
+    public static void main(String[] args) 
+    {
+        
+    }
+    
+}
